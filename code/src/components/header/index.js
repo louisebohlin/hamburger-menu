23be8f525f0c37732269732e975bae56ba31145a
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-10-01 15:15:22 +0200

    Almost done except styling

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -8,6 +8,16 @@ class Header extends React.Component {
   render() {
     return (
       <header>
+      <label for="hamburger" class="hamburger-label" >
+          <div className="hamburger-divs"></div>
+          <div className="hamburger-divs"></div>
+          <div className="hamburger-divs"></div>
+      </label>
+      <input
+        id="hamburger"
+        class="hamburger-checkbox"
+        type="checkbox"
+        />
         <Grid className="header-grid">
           <a href="http://technigo.io">Technigo</a>
           <a href="http://technigo.io">Boot Camp</a>
